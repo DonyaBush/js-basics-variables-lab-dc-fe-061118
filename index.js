6403490a5c20fd77b3bdc1cd453a77e9cc12ad22
Ki Ki  Bush <donyabush@gmail.com>
2018-07-18 23:32:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,26 +2,17 @@
 
 
 var companyName = 'Scuber';
-it('is set as Scuber', function () {
-  expect(companyName).to.equal('Scuber');
-});
 
- var companyName ='Scuber';
- it('does not raise error if the Scuber is changed', function () {
-    expect(companyName).to.throw(error);
-    });
+
+var companyName ='Scuber';
+ 
 
 
 var mostProfitableNeighborhood = 'Chelsea';
-it('is set as chelsea', function () {
-  expect(mostProfitableNeighborhood).to.equal('Chelsea');
-});
 
 
 var companyCeo = 'Susan Smith';
-it('is set as Susan Smith', function () {
-  expect(companyCeo).to.equal('Susan Smith');
-});
+
 
 
 
