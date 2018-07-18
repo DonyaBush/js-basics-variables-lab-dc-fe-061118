c2cf17a869c3c5de94d00f7c7d4d94935101c89f
Ki Ki  Bush <donyabush@gmail.com>
2018-07-18 23:28:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+
+
+var companyName = 'Scuber';
+it('is set as Scuber', function () {
+  expect(companyName).to.equal('Scuber');
+});
+
+ var companyName ='Scuber';
+ it('does not raise error if the Scuber is changed', function () {
+      expect(function () { Scruber = 'Scruber' }).to.not.throw(TypeError);
+    });
+
+
+var mostProfitableNeighborhood = 'Chelsea';
+it('is set as chelsea', function () {
+  expect(mostProfitableNeighborhood).to.equal('Chelsea');
+});
+
+
+var companyCeo = 'Susan Smith';
+it('is set as Susan Smith', function () {
+  expect(companyCeo).to.equal('Susan Smith');
+});
+
+
+
+
+
